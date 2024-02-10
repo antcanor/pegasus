95bf8e40597453ab0d24f58cb282059219bcae42
pedrodpf333 <2908765@alu.murciaeduca.es>
2024-02-10 12:41:46 +0100

    pausaaa

--- assets/js/control_video.js
+++ assets/js/control_video.js
@@ -2,6 +2,33 @@ const video=document.querySelector("#video")
 const botonSonido = document.querySelector("#botonSonido")
 const botonPausa = document.querySelector("#botonPausa")
 
+
+// Agrega esta función
+video.addEventListener('click', function() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+});
+
+// Funciones existentes para el control de sonido y pausa
+function toggleMute() {
+    if(video.muted) {
+        video.muted = false;
+    } else {
+        video.muted = true;
+    }
+}
+
+function togglePause() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
 function toggleMute(){
     if(video.muted){
         video.muted=false
